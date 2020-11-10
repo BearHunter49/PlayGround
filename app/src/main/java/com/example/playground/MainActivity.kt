73bbf6a69cdfca36c4663cd209def46bc0dd58fa
BearHunter49 <rkdcjf987@naver.com>
2020-11-10 20:12:12 +0900

    implemented Fullscreen (API 30 or below)

---
+++ app/src/main/java/com/example/playground/MainActivity.kt
@@ -0,0 +1,92 @@
+package com.example.playground
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.view.WindowInsets
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private var isFull: Boolean = false
+    private val sdkLevel: Int = Build.VERSION.SDK_INT
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+//        View.OnSystemUiVisibilityChangeListener {
+//            Toast.makeText(this, "Full screen: $isFull", Toast.LENGTH_SHORT).show()
+//        }
+
+        Toast.makeText(this, "Your API is $sdkLevel", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.home_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.full_btn -> {
+                if (isFull) {
+                    item.icon = ContextCompat.getDrawable(this, R.drawable.ic_fullscreen_black_24dp)
+                    showSystemUI()
+                } else {
+                    item.icon =
+                        ContextCompat.getDrawable(this, R.drawable.ic_fullscreen_exit_black_24dp)
+                    hideSystemUI()
+                }
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) {
+            if (isFull) hideSystemUI()
+        }
+    }
+
+    private fun hideSystemUI() {
+        if (sdkLevel >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false)
+            window.insetsController?.hide(WindowInsets.Type.systemBars())
+        } else {
+            window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
+                    // SystemBar 밑에도 표시되도록 함
+                    or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    // Hide the nav bar and status bar
+                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_FULLSCREEN)
+        }
+        isFull = true
+    }
+
+    private fun showSystemUI() {
+        if (sdkLevel >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(true)
+            window.insetsController?.show(WindowInsets.Type.systemBars())
+        } else {
+            // STABLE 플래그로 인해 다시 resize 되지 않음
+            window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
+        }
+        isFull = false
+    }
+
+}
